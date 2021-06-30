4b90ec4ccfdadad3c09704131690616501360521
Hugothms <hugothms@gmail.com>
2021-06-30 12:44:12 +0200

    change include path

--- include_your_files.hpp
+++ include_your_files.hpp
@@ -6,17 +6,17 @@
 /*   By: hthomas <hthomas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/08 11:23:23 by llefranc          #+#    #+#             */
-/*   Updated: 2021/06/09 15:23:46 by hthomas          ###   ########.fr       */
+/*   Updated: 2021/06/30 12:19:08 by hthomas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef INCLUDE_YOUR_FILES_HPP
 #define INCLUDE_YOUR_FILES_HPP
 
-#include "../ft_containers/containers/vector.hpp"
-#include "../ft_containers/containers/list.hpp"
-#include "../ft_containers/containers/stack.hpp"
-#include "../ft_containers/containers/queue.hpp"
-#include "../ft_containers/containers/map.hpp"
+#include "../containers/vector.hpp"
+#include "../containers/list.hpp"
+#include "../containers/stack.hpp"
+#include "../containers/queue.hpp"
+#include "../containers/map.hpp"
 
 #endif
